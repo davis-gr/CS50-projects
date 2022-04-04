a7cd54d556a0d0a2402cd9814a0a0491aafc9678
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-04-04 13:31:31 +0000

    Mon, Apr 4, 2022, 1:31 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -9,5 +9,8 @@ int main(void)
         n = get_long("Number: ");
     }
     // Reject anything else besides numbers between 1 and 8
+    // Amex - 15 digits, starts with 34 or 37
+    // MC - 16 digits, starts with 51-55
+    // VISA - 13 or 16 digits, starts with 4
     while (len(n) < );
 }
