98b43b2756377972c1610f0847373bcb672e008f
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-04-04 13:26:34 +0000

    Mon, Apr 4, 2022, 1:26 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,7 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    }
+    // Reject anything else besides numbers between 1 and 8
+    while (n < 1 || n > 8);
 }
