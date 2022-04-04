223a106694a247dac5b2b4507b28e13c7b28b008
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-04-04 13:32:12 +0000

    Mon, Apr 4, 2022, 1:32 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -12,5 +12,5 @@ int main(void)
     // Amex - 15 digits, starts with 34 or 37
     // MC - 16 digits, starts with 51-55
     // VISA - 13 or 16 digits, starts with 4
-    while (len(n) < );
+    while (len(n) < 13);
 }
