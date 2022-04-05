16f9ed0538c8f7e6879af2be97852c007c51f304
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-04-05 09:50:29 +0000

    Tue, Apr 5, 2022, 9:50 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -4,19 +4,21 @@
 
 int main(void)
 {
-    long n, n2, n3, n4;
+    long credNumber;
     //do
     {
-        n = get_long("Number: ");
-        n2 = n % 10;
-        n3 = n % 100 / 10 % 10;
-        n4 = n % 1000 / 100 % 100;
+        credNumber = get_long("Number: ");
+        //n2 = n % 10;
+        //n3 = n % 100 / 10 % 10;
+        //n4 = n % 1000 / 100 % 100;
     }
     // Amex - 15 digits, starts with 34 or 37
     // MC - 16 digits, starts with 51-55
     // VISA - 13 or 16 digits, starts with 4
     //while (strlen(n) < 13);
     {
+        for (int i = 1; i <= n; i * 10))
+        
         printf("%li\n", n);
         printf("%li\n", n2);
         printf("%li\n", n3);
