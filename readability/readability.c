1358a36f086fc787e641a3f0cded411cdfe3f373
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-04-07 12:21:04 +0000

    Thu, Apr 7, 2022, 12:21 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,25 +1,28 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 int count_letters(string text);
 
 int main(void)
 {
     string fullText = get_string("Text: ");
-    printf("%s", fullText)
-    printf("%i letters", letterCount)
+    printf("%s\n", fullText);
+    int letterCount = count_letters(fullText);
+    printf("%i letters\n", letterCount);
 }
 
 int count_letters(string text)
 {
-    int letterCount = 0
+    int count = 0;
     for (int i = 0, n = strlen(text); i < n; i++)
     {
         int charNum = tolower(text[i]);
         if (charNum >= 97 && charNum <= 122)
             {
-                letterCount++
+                count++;
             }
     }
+    return count;
 }
