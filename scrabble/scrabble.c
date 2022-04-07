098cf6a3abbddf1d9fca39959716f4b8f4159072
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-04-07 11:33:27 +0000

    Thu, Apr 7, 2022, 11:33 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -12,11 +12,11 @@ int main(void)
 {
     // Get input words from both players
     string word1 = get_string("Player 1: ");
-    string word2 = get_string("Player 2: ");
+    //string word2 = get_string("Player 2: ");
 
     // Score both words
     int score1 = compute_score(word1);
-    int score2 = compute_score(word2);
+    //int score2 = compute_score(word2);
 
     // TODO: Print the winner
 }
@@ -26,11 +26,10 @@ int compute_score(string word)
     //Compute and return score for string
     for (int i = 0, n = strlen(word); i < n; i++)
     {
-        if (s[i] >= 'a' && s[i] <= 'z')
-        {
-            printf("%c", s[i] - 32);
-        }
-        else
-        {
-            printf("%c", s[i]);
+        int charNum = word[i];
+        printf("%i", tolower(charNum));
+        printf("\n");
+    }
+    printf("\n");
+    return 1;
 }
