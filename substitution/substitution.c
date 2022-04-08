4ae8e3e281e71424d5c57cc1af27ecd36a627b85
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-04-08 07:26:01 +0000

    Fri, Apr 8, 2022, 7:26 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -5,23 +5,24 @@
 
 string encrypt(string text);
 
+string cipher = argv[0];
+
 int main(int argc, string argv[])
 {
     string plaintext = get_string("plaintext: ");
     printf("%s", argv[0]);
-//    string ciphertext = encrypt(plaintext);
-//    string cipher = argv[];
-//    printf("%s", ciphertext);
+    string ciphertext = encrypt(plaintext);
+    printf("%s", ciphertext);
 }
 
-/*string encrypt(string text)
+string encrypt(string text)
 {
     string newtext = text;
     for (int i = 0, n = strlen(text); i < n; i++)
     {
         int charNum = tolower(text[i]);
         //printf("%i", tolower(charNum));
-        char newChar = argv[charNum - 97];
+        char newChar = cipher[charNum - 97];
         //printf(" - %i", pts);
         //printf("\n");
         if (charNum >= 97 && charNum <= 122)
@@ -32,7 +33,7 @@ int main(int argc, string argv[])
 //    printf("%i", totalPts);
 //    printf("\n");
     return newtext;
-}*/
+}
 
 
 // get key
