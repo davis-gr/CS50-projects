4591b4fd15aff9b787f01adebc689a972715df64
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-04-08 06:55:27 +0000

    Fri, Apr 8, 2022, 6:55 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,7 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
+{
+    string plaintext = get_string("plaintext: ");
+    //string ciphertext =
+}
+
+string encrypt(string text)
 {
 
 }
