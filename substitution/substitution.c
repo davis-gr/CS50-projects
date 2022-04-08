69db436816da58dc9d47078d24631bf09f0d4343
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-04-08 10:45:56 +0000

    Fri, Apr 8, 2022, 10:45 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -9,13 +9,16 @@ string cipher = "";
 
 int main(int argc, string argv[])
 {
+    //Handles too many or too few command line arguments
     if (argc != 2)
     {
         printf("Enter a key and only one key!\n");
         return 1;
     }
+    //cipher holds the key from command line
     cipher = argv[1];
-    //printf("%lu\n", strlen(cipher));
+
+    //handles too short keys
     if (strlen(cipher) < 26)
     {
         printf("Enter a key of exactly 26 unique English letters!\n");
@@ -23,16 +26,18 @@ int main(int argc, string argv[])
     }
     for (int i = 0, n = strlen(cipher); i < n; i++)
     {
+        //this tolower part handles upper and lower case duplicates (e.g. A & a)
         int singleLetterI = tolower(cipher[i]);
+        //handles non-alpha characters
         if (isalpha(cipher[i]) == 0)
         {
-            //printf("%i", suds);
             printf("Enter a key of exactly 26 unique English letters!\n");
             return 1;
         }
-        for(int j = i + 1; j < strlen(cipher); j++)
+        for (int j = i + 1; j < strlen(cipher); j++)
         {
             int singleLetterJ = tolower(cipher[j]);
+            // handles duplicate characters
             if (singleLetterI == singleLetterJ)
             {
                 printf("Enter a key of exactly 26 unique English letters!\n");
@@ -41,7 +46,6 @@ int main(int argc, string argv[])
         }
     }
     string plaintext = get_string("plaintext: ");
-//    printf("%s\n", cipher);
     string ciphertext = encrypt(plaintext);
     printf("ciphertext: %s\n", ciphertext);
 }
@@ -52,11 +56,13 @@ string encrypt(string text)
     for (int i = 0, n = strlen(text); i < n; i++)
     {
         int charNum = text[i];
+        //handles lower case alpha text replacement
         if (islower(text[i]) && isalpha(text[i]))
         {
             char newChar = tolower(cipher[charNum - 'a']);
             newtext[i] = newChar;
         }
+        //handles upper case alpha text replacement
         else if (isupper(text[i]) && isalpha(text[i]))
         {
             char newChar = toupper(cipher[charNum - 'A']);
