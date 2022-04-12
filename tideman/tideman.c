a2df28a7bb6cf7491b351936fb5fdfe326d992f5
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-04-12 06:55:21 +0000

    Tue, Apr 12, 2022, 6:55 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -100,7 +100,7 @@ int main(int argc, string argv[])
 // Update ranks given a new vote
 bool vote(int rank, string name, int ranks[])
 {
-    for (int rank = candidate_count; rank >= 0; rank--)
+    for (int i = 0; i < candidate_count; i++)
     {
         if (strcmp(name, candidates[i]) == 0)
         {
