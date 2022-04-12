33bbb7d1d8eb9cbbb02a41f85d74b100fae90789
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-04-12 18:23:54 +0000

    Tue, Apr 12, 2022, 6:23 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -220,7 +220,13 @@ void lock_pairs(void)
 // Print the winner of the election
 void print_winner(void)
 {
-    for (int i = 0; i < candidate_count)
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = 0; j < candidate_count; j++)
+        {
+            if 
+        }
+    }
     printf("%s\n", candidates[i])
     return;
 }
