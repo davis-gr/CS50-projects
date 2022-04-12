fb517664bd59b505148a4d36cb501b65336d735c
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-04-12 11:39:40 +0000

    Tue, Apr 12, 2022, 11:39 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -159,13 +159,10 @@ void add_pairs(void)
 // Sort pairs in decreasing order by strength of victory
 void sort_pairs(void)
 {
-    int points, i;
+    int maxPoints, i;
     for (i = 0; i < pair_count; i++)
     {
-        points = preferences[pairs[i].winner][pairs[i].loser];
-        
-
-
+        maxPoints = preferences[pairs[i].winner][pairs[i].loser];
         for (int j = i+1; j < pair_count; j++)
         {
             if (preferences[pairs[j].winner][pairs[j].loser] > maxPoints)
