7506e750638121731823d1a693f5d1fc4b283bda
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-04-12 08:10:51 +0000

    Tue, Apr 12, 2022, 8:10 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -132,7 +132,10 @@ void record_preferences(int ranks[])
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = i+1)
+    }
     return;
 }
 
