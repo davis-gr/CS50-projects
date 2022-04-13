d14b38da60ec60ba2e17f8e8559137284301a8f9
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-04-13 06:54:07 +0000

    Wed, Apr 13, 2022, 6:54 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -182,7 +182,7 @@ int find_min(void)
     int minVotes = candidates[0].votes;
     for (int i = 0; i < candidate_count - 1; i++)
         {
-            if (candidates[i].votes < minVotes)
+            if (candidates[i].votes < minVotes && !candidates[i].eliminated)
                 {
                     minVotes = candidates[i].votes;
                 }
