5cc0b1edb262105a8dcfb9ecc492805ef7ddce69
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-04-13 06:28:14 +0000

    Wed, Apr 13, 2022, 6:28 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -134,7 +134,6 @@ bool vote(int voter, int rank, string name)
         {
             if (strcmp(name, candidates[j].name) == 0)
             {
-                candidates[j].votes++;
                 preferences[i][rank] = j;
                 rank++;
                 return true;
@@ -149,12 +148,12 @@ void tabulate(void)
 {
     for (int i = 0; i < voter_count; i++)
         {
-            for int (j = 0; j < candidate_count; j++)
+            for (int j = 0; j < candidate_count; j++)
             {
-                int preferredCandidate = preferences[i][j]
+                int preferredCandidate = preferences[i][j];
                 if (!candidates[preferredCandidate].eliminated)
                 {
-                    
+                    candidates[preferredCandidate].votes++;
                 }
             }
         }
