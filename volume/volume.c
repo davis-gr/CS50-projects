c2e8a4944d4fd1f329cc1c926224fa6f1b54b854
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-04-13 13:35:33 +0000

    Wed, Apr 13, 2022, 1:35 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -37,10 +37,8 @@ int main(int argc, char *argv[])
 
     uint8_t inputHeader[HEADER_SIZE];
 
-    for (int i = 0; i < HEADER_SIZE; i++)
-    {
-        fread
-    }
+    fread
+
 
     // TODO: Read samples from input file and write updated data to output file
 
