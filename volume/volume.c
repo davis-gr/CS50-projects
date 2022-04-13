95bde10dd4782fefc58869eab9de20b6036a3320
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-04-13 13:33:40 +0000

    Wed, Apr 13, 2022, 1:33 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -34,10 +34,12 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
-    uint8_t inputHeader[];
+
+    uint8_t inputHeader[HEADER_SIZE];
+
     for (int i = 0; i < HEADER_SIZE; i++)
     {
-        
+        fread
     }
 
     // TODO: Read samples from input file and write updated data to output file
