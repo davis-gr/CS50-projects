63b7ad08e998e5c708f0488db2d9947f49ad40d1
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-04-13 06:34:57 +0000

    Wed, Apr 13, 2022, 6:34 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -164,7 +164,10 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+        {
+            int votesNeeded = voter_count / 2 + 1
+        }
     return false;
 }
 
