c2a13a3caccf0ea3b46bbc9d96ac776cdcc80586
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-04-28 11:03:18 +0000

    Thu, Apr 28, 2022, 11:03 AM +00:00

---
+++ filter-more/helpers.c
@@ -0,0 +1,33 @@
+#include "helpers.h"
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    float gValue;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            gvalue 
+        }
+    }
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Detect edges
+void edges(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
