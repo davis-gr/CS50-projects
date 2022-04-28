57bdca78d0ccfd83a6dca906b57bff4dbd817b32
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-04-28 16:04:51 +0000

    Thu, Apr 28, 2022, 4:04 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -146,43 +146,43 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                         // middle top
                         else if (k == 0 && l == -1)
                         {
-                        xRed += image[l+j][i+k].rgbtRed * -2;
-                        xGreen += image[l+j][i+k].rgbtGreen * -2;
-                        xBlue += image[l+j][i+k].rgbtBlue * -2;
+                        yRed += image[l+j][i+k].rgbtRed * -2;
+                        yGreen += image[l+j][i+k].rgbtGreen * -2;
+                        yBlue += image[l+j][i+k].rgbtBlue * -2;
                         }
                         // middle bottom
                         else if (k == 0 && l == 1)
-                        {
-                        xRed += image[l+j][i+k].rgbtRed * 2;
-                        xGreen += image[l+j][i+k].rgbtGreen * 2;
-                        xBlue += image[l+j][i+k].rgbtBlue * 2;
-                        }
-                        // right top
-                        else if (k == 1 && l == -1)
-                        {
-                        yRed += image[l+j][i+k].rgbtRed;
-                        yGreen += image[l+j][i+k].rgbtGreen;
-                        yBlue += image[l+j][i+k].rgbtBlue;
-                        xRed += image[l+j][i+k].rgbtRed * -1;
-                        xGreen += image[l+j][i+k].rgbtGreen * -1;
-                        xBlue += image[l+j][i+k].rgbtBlue * -1;
-                        }
-                        // right middle
-                        else if (k == 1 && l == 0)
                         {
                         yRed += image[l+j][i+k].rgbtRed * 2;
                         yGreen += image[l+j][i+k].rgbtGreen * 2;
                         yBlue += image[l+j][i+k].rgbtBlue * 2;
                         }
+                        // right top
+                        else if (k == 1 && l == -1)
+                        {
+                        xRed += image[l+j][i+k].rgbtRed;
+                        xGreen += image[l+j][i+k].rgbtGreen;
+                        xBlue += image[l+j][i+k].rgbtBlue;
+                        yRed += image[l+j][i+k].rgbtRed * -1;
+                        yGreen += image[l+j][i+k].rgbtGreen * -1;
+                        yBlue += image[l+j][i+k].rgbtBlue * -1;
+                        }
+                        // right middle
+                        else if (k == 1 && l == 0)
+                        {
+                        xRed += image[l+j][i+k].rgbtRed * 2;
+                        xGreen += image[l+j][i+k].rgbtGreen * 2;
+                        xBlue += image[l+j][i+k].rgbtBlue * 2;
+                        }
                         // right bottom
                         else if (k == 1 && l == 1)
                         {
-                        yRed += image[l+j][i+k].rgbtRed;
-                        yGreen += image[l+j][i+k].rgbtGreen;
-                        yBlue += image[l+j][i+k].rgbtBlue;
-                        xRed += yRed;
-                        xGreen += yGreen;
-                        xBlue += yBlue;
+                        xRed += image[l+j][i+k].rgbtRed;
+                        xGreen += image[l+j][i+k].rgbtGreen;
+                        xBlue += image[l+j][i+k].rgbtBlue;
+                        yRed += xRed;
+                        yGreen += xGreen;
+                        yBlue += xBlue;
                         }
                     }
                 }
