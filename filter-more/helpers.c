bbca082916668144c9a77809177727460d0487ce
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-04-28 11:56:46 +0000

    Thu, Apr 28, 2022, 11:56 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -30,6 +30,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
             gValue = image[i][j].rgbtGreen;
             bValue = image[i][j].rgbtBlue;
         }
+        for (int k = )
     }
     return;
 }
