5e92c1f09dcd7ed1fe3746a40301ce553a1e817d
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-04-28 13:13:31 +0000

    Thu, Apr 28, 2022, 1:13 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -55,6 +55,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             rBlur = round((image[i-1][j-1].rgbtRed + image[i-1][j].rgbtRed + image[i-1][j+1].rgbtRed +
                     image[i][j-1].rgbtRed + image[i][j].rgbtRed + image[i][j+1].rgbtRed +
                     image[i+1][j-1].rgbtRed + image[i+1][j].rgbtRed + image[i+1][j+1].rgbtRed) / 9);
+            rBlur = round((image[i-1][j-1].rgbtRed + image[i-1][j].rgbtRed + image[i-1][j+1].rgbtRed +
+                    image[i][j-1].rgbtRed + image[i][j].rgbtRed + image[i][j+1].rgbtRed +
+                    image[i+1][j-1].rgbtRed + image[i+1][j].rgbtRed + image[i+1][j+1].rgbtRed) / 9);
         }
     }
     return;
