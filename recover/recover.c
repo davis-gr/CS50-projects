5bb95f7749b60522002834efd73f16e6a7b23644
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-04-29 07:19:29 +0000

    Fri, Apr 29, 2022, 7:19 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -7,7 +7,8 @@ int main(int argc, char *argv[])
 {
     FILE *file = fopen(argv[1], "r");
 
-    while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
+    //deffine buffer;
+    while (fread(buffer, sizeof(BYTE), 512, file) == BLOCK_SIZE)
     {
 
 
