9c5ebfe84290cbfd96a1c03cf45d5d552eec66cc
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-04-29 11:26:01 +0000

    Fri, Apr 29, 2022, 11:26 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
             if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
             {
                 FILE *img = fopen(filename, "w");
-                fwrite(buffer, sizeof(BYTE), 512, filename);
+                fwrite(buffer, sizeof(BYTE), BLOCK_SIZE, filename);
             }
         }
     }
