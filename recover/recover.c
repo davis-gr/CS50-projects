5d0cb21b8b1501d8fc85223bd41bebee6f863d34
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-04-29 07:27:08 +0000

    Fri, Apr 29, 2022, 7:27 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -18,3 +18,17 @@ int main(int argc, char *argv[])
     fwrite(buffer, sizeof(BYTE), 512, filename);
     FILE *img = fclose(filename);
 }
+
+
+// open memory card
+// repeat until end of card:
+    //read 512 bytes in buffer
+    // If start of new JPEG
+        // If first JPEG
+        //...
+        // Else
+        //...
+    //Else
+        // If already found JPEG
+        //...
+//Close any remaining files
