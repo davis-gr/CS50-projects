3727cb4447c0706093ef0d1a6e330417a5053efe
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-05-03 14:13:24 +0000

    Tue, May 3, 2022, 2:13 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -47,7 +47,7 @@ unsigned int hash(const char *word)
     int hujovinka = 0;
     int n = 26;
     hujovinka += toupper(word[0]) - 'A');
-    hujovinka += toupper(word[1])*n - 'A'*n);
+    hujovinka += toupper(word[1])*n - 'A'*n - n);
     return hujovinka;
 }
 
