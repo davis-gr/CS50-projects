4aba8a0f316e0e676c1b571492399812cfc09a0f
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-05-03 12:08:55 +0000

    Tue, May 3, 2022, 12:08 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -82,7 +82,7 @@ bool load(const char *dictionary)
         }
         else
         {
-            n->next = table[hvalue]->next;
+            n->next = table[hvalue]->word;
             table[hvalue] = n;
         }
         wordCount++;
