e161de42624c54ff4eda9276585a74378a27092f
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-05-06 14:14:48 +0000

    Fri, May 6, 2022, 2:14 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -5,7 +5,6 @@ def count_letters(text):
     for i in text:
         if i.isalpha() == True:
             letterCount += 1
-    print(letterCount)
     return letterCount
 
 def count_words(text):
@@ -13,12 +12,15 @@ def count_words(text):
     for i in text:
         if i.isspace() == True:
             wordCount += 1
-    print(wordCount)
     wordCount += 1
-    return wordCount
+    return wordCount+1
 
 def count_sentences(text):
-    
+    sentenceCount = 0
+    for i in text:
+        if i == chr(46) or i == chr(33) or i == chr(63):
+            sentenceCount += 1
+    return sentenceCount
 
 L = count_letters(fullText) / count_words(fullText) * 100
 S = count_sentences(fullText) / count_words(fullText) * 100
