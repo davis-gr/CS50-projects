5367a82ffa5cce6f3650cd2350f7ffa892d358c7
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-05-06 14:02:58 +0000

    Fri, May 6, 2022, 2:02 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -11,4 +11,8 @@ S = sentenceCount / wordCount * 100
 def count_letters(text):
     count = 0
     for i in text:
-        
+        if i.isalpha() == True:
+            count += 1
+    print(count)
+
+count_letters(fullText)
