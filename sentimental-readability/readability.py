12df7f897d5dbefbf630fb1fca33938054981276
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-05-06 14:04:38 +0000

    Fri, May 6, 2022, 2:04 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -5,8 +5,8 @@ letterCount = 0
 wordCount = 0
 sentenceCount = 0
 
-L = letterCount / wordCount * 100
-S = sentenceCount / wordCount * 100
+#L = letterCount / wordCount * 100
+#S = sentenceCount / wordCount * 100
 
 def count_letters(text):
     count = 0
@@ -15,4 +15,16 @@ def count_letters(text):
             count += 1
     print(count)
 
+def count_sentences(text):
+    count = 0
+
+
+
+
+
 count_letters(fullText)
+
+
+#count_words(fullText)
+#count_sentences(fullText)
+
