1774db251220d86f3ea56e89da8e666d08916867
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-05-06 12:51:33 +0000

    Fri, May 6, 2022, 12:51 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -6,5 +6,13 @@ cardNumStr = str(cardNum)
 cardNumRev = cardNumStr[::-1]
 cardNumLen = len(cardNumStr)
 
+sumEven = 0
+sumOdd = 0
+sum = 0
+
 for i in cardNumRev[1::2]:
-    
+    sumEven += int(i)*2
+    sumEvenStr = str(sumEven)
+    print(sumEvenStr)
+#for i in cardNumRev[0::2]:
+    #sumOdd += i
