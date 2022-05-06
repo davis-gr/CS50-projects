23b5b7b62fecc89769d9d4b60081d511d7262d1c
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-05-06 13:34:09 +0000

    Fri, May 6, 2022, 1:34 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -26,3 +26,9 @@ if sum % 10 == 0:
         print("VISA")
     elif cardNumStr[0] == '4' and (cardNumStr[1] == '4' or cardNumStr[1] == '7') and cardNumLen == 15:
         print("AMEX")
+    elif cardNumStr[0] == '5' and (cardNumStr[1] == '1' or cardNumStr[1] == '2' or cardNumStr[1] == '3' or cardNumStr[1] == '4' or cardNumStr[1] == '5') and cardNumLen == 16:
+        print("MASTERCARD")
+    else:
+        print("INVALID")
+else:
+    print("INVALID")
