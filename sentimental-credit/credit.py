99c68851c7a77dff60f5619c53ce7eab1e830f49
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-05-06 13:02:11 +0000

    Fri, May 6, 2022, 1:02 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -3,20 +3,17 @@ from cs50 import get_int
 cardNum = get_int("Number: ")
 
 cardNumStr = str(cardNum)
-cardNumRev = cardNumStr[::-1]
+cardNumReverse = cardNumStr[::-1]
 cardNumLen = len(cardNumStr)
 
-evenDigits = []
+sumEven = 0
 sumOdd = 0
 sum = 0
 prodEven = 0
 
-for i in cardNumRev[1::2]:
-    sumEven += int(i)*2
-    sumEvenStr = str(sumEven)
-    for j in sumEvenStr:
-        prodEven += int(j)
-
-print(sumEvenStr)
-#for i in cardNumRev[0::2]:
-    #sumOdd += i
+for i in cardNumReverse[1::2]:
+    intI = int(i)*2
+    strI = str(intI)
+    for j in strI:
+        sumEven += int(strI[j])
+print(sumEven)
