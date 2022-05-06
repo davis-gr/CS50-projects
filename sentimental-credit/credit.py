ecc82ad8325820c3e176980797873f7e6a15bfbd
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-05-06 12:44:52 +0000

    Fri, May 6, 2022, 12:44 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -3,6 +3,7 @@ from cs50 import get_int
 cardNum = get_int("Number: ")
 
 cardNumStr = str(cardNum)
+cardNumLen = len(cardNum)
 
-for i in cardNumStr:
-    
+if cardNumLen % 2 == 0:
+    for i in cardNumStr:
