36da6c52a9a336a3cdc9640bc779acec7f663f39
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-05-06 13:31:01 +0000

    Fri, May 6, 2022, 1:31 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -22,7 +22,7 @@ for k in cardNumReverse[0::2]:
 sum = sumEven+sumOdd
 
 if sum % 10 == 0:
-    if cardNumStr[0] == 4 and (cardNumLen == 13 or cardNumLen == 16):
+    if cardNumStr[0] == '4' and (cardNumLen == 13 or cardNumLen == 16):
         print("VISA")
-    elif cardNumStr[0] == 4 and (cardNumStr[1] == 4 or cardNumStr[1] == 7) and cardNumLen == 15:
+    elif cardNumStr[0] == '4' and (cardNumStr[1] == '4' or cardNumStr[1] == '7') and cardNumLen == 15:
         print("AMEX")
