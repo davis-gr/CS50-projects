db06905d98bdcd9068f022736e7df46f34f45578
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-05-06 09:05:57 +0000

    Fri, May 6, 2022, 9:05 AM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -58,7 +58,9 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
-    
+    while len(teams) > 1:
+        simulate_round(teams)
+        
 
 
 if __name__ == "__main__":
