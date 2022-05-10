a76fd11ff88ffa68cc316d7305f4f4e0684fba4b
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-05-10 08:37:58 +0000

    Tue, May 10, 2022, 8:37 AM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,5 +1,8 @@
-select count(*) from movies
+select
+
+select title, count(*) from movies
 join stars on movies.id = stars.movie_id
 join people on stars.person_id = people.id
 where people.name in ('Johnny Depp','Helena Bonham Carter')
+group by title
 having count(*) > 1;
