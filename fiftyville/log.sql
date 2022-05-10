98313765647a139d1f786ef11f8005ffa03be47f
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-05-10 09:30:46 +0000

    Tue, May 10, 2022, 9:30 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -21,3 +21,54 @@ and day = 28
 and activity = 'exit'
 and hour = 10
 and minute between 15 and 25;
+
+-- find all ATM withdrawals from this day at Leggett Street
+select account_number from atm_transactions
+where year = 2021
+and month = 7
+and day = 28
+and atm_location = 'Leggett Street'
+and transaction_type = 'withdraw';
+
+-- find all outgoing phone calls with duration < 1 minute and next to the time of theft
+select caller from phone_calls
+where year = 2021
+and month = 7
+and day = 28
+and duration < 60;
+
+-- find the passenger passport numbers of the earliest flight out of fiftyville tomorrow
+
+select passport_number from passengers
+where flight_id in (select flights.id from flights
+                    join airports on flights.origin_airport_id = airports.id
+                    where year = 2021
+                    and month = 7
+                    and day = 29
+                    and airports.city = 'Fiftyville'
+                    order by hour, minute
+                    limit 1);
+
+-- find out who the suspect is
+select * from people
+where phone_number in (select caller from phone_calls
+                        where year = 2021
+                        and month = 7
+                        and day = 28
+                        and duration < 60)
+and license_plate in (select license_plate from bakery_security_logs
+                        where year = 2021
+                        and month = 7
+                        and day = 28
+                        and activity = 'exit'
+                        and hour = 10
+                        and minute between 15 and 25)
+and passport_number in (select passport_number from passengers
+                        where flight_id in (select flights.id from flights
+                                            join airports on flights.origin_airport_id = airports.id
+                                            where year = 2021
+                                            and month = 7
+                                            and day = 29
+                                            and airports.city = 'Fiftyville'
+                                            order by hour, minute
+                                            limit 1));
