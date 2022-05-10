50a6ec0d1762614757a47320c7ecf8c6462023d3
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-05-10 08:30:28 +0000

    Tue, May 10, 2022, 8:30 AM +00:00

--- movies/9.sql
+++ movies/9.sql
@@ -3,4 +3,4 @@ join stars on people.id = stars.person_id
 join movies on stars.movie_id = movies.id
 where movies.year = 2004
 and people.birth is not null
-order by people.birth desc;
+order by people.birth;
