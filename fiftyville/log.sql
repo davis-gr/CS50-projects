13a97685453ba708544a5c17e927411468fba2d5
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-05-10 09:13:31 +0000

    Tue, May 10, 2022, 9:13 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,5 +1,5 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
--- Understand what was the crime that happened
+-- Understand what was the crime that happened.
 select description from crime_scene_reports
 where year = 2021
 and month = 7
@@ -12,3 +12,9 @@ where year = 2021
 and month = 7
 and day = 28
 and transcript like '%bakery%';
+
+-- find all car exits that happened after 10:15am
+select * from bakery_security_logs
+where year = 2021
+and month = 7
+and day = 28;
