7bfb0ff31bce9240e38132a9531f39e361817b6d
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-05-10 09:07:36 +0000

    Tue, May 10, 2022, 9:07 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,12 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
+-- Understand what was the crime that happened
+select description from crime_scene_reports
+where year = 2021
+and month = 7
+and day = 28
+and street = 'Humphrey Street';
 
+select * from interviews
+where year = 2021
+and month = 7
+and day = 28;
