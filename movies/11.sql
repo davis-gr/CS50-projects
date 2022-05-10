92499ed460d5e90b270bbe785d00197da592c758
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-05-10 08:32:49 +0000

    Tue, May 10, 2022, 8:32 AM +00:00

---
+++ movies/11.sql
@@ -0,0 +1,7 @@
+select title from movies
+join stars on movies.id = stars.movie_id
+join people on people.id = stars.person_id
+join ratings on movies.id = ratings.movie_id
+where people.name = 'Chadwick Boseman'
+order by ratings.rating desc
+limit 5;
