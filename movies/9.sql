6e4e76197b4b1accf07de7ecc108d1f88cd5e71c
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-05-10 08:28:06 +0000

    Tue, May 10, 2022, 8:28 AM +00:00

---
+++ movies/9.sql
@@ -0,0 +1,6 @@
+select distinct name from people
+join stars on people.id = stars.person_id
+join movies on stars.movie_id = movies.id
+where movies.year = 2004
+and people.birth is not null
+order by people.birth desc;
