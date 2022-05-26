22824fe4b8527fb121f032782e289882d541ccee
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-05-26 12:52:41 +0000

    Thu, May 26, 2022, 12:52 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -60,8 +60,9 @@ def buy():
         elif quotes["price"] * shareCount > userCash[0]['cash']:
             return apology("not enough $$$")
         else:
+            remainingCash = userCash[0]['cash'] - quotes["price"] * shareCount
             db.execute("INSERT INTO transactions (user_id, type, ticker, share_count, share_price, datetime) VALUES(?, 'BUY', ?, ?, ?, datetime('now'))", session["user_id"], quotes["symbol"], shareCount, quotes["price"])
-            db.execute("UPDATE users SET cash = ())
+            db.execute("UPDATE users SET cash = ?", remainingCash)
             return render_template("index.html")
     else:
         return render_template("buy.html")
