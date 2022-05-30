c961614fb5baf7d7258f77a6dace4b9f21b877f9
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-05-30 13:56:45 +0000

    Mon, May 30, 2022, 1:56 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -230,8 +230,10 @@ def change_password():
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+        if not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid old password entered", 403)
+        elif check_password_hash(rows[0]["hash"], request.form.get("password")):
+            
         return redirect("/", flash('Password changed!'))
     else:
         return render_template("password.html")
