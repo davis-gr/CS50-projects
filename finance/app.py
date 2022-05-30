24c053d2ce2f6424e35beb0d1b762950c19e276d
davis-gr <102602289+davis-gr@users.noreply.github.com>
2022-05-30 11:07:51 +0000

    Mon, May 30, 2022, 11:07 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -44,8 +44,11 @@ def after_request(response):
 def index():
     portfolio = db.execute("SELECT ticker, sum(share_count) as shareCount FROM transactions WHERE user_id = ? GROUP BY ticker", session["user_id"])
     print(portfolio)
+    currentPrices = []
     for stock in portfolio:
-        portfolio.append(lookup(stock["ticker"]))
+        currentPrices.append(lookup(stock["ticker"]))
+    print(currentPrices)
+
     return render_template("index.html", portfolio = portfolio)
 
 
